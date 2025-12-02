d30f218f8a96a622310960a623290b1b2b4e5b05
Yakk125 <rohmatullaharya@gmail.com>
2025-12-02 15:18:36 +0700

    Fix merge after stash

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 $severname = "localhost";
 $username = "root";
@@ -13,3 +14,20 @@ if (!$conn) {
 
 echo "Koneksi Berhasil";
 ?>
+=======
+<?php
+$severname = "localhost";
+$username = "root";
+$password = "";
+$database = "db_data";
+// Create connection
+$conn = mysqli_connect(hostname: $severname, username: $username, password: $password, database: $database);
+
+// Check connection
+if (!$conn) {
+    die("Koneksi Gagal: " . mysqli_connect_error());
+}
+
+echo "Koneksi Berhasil";
+?>
+>>>>>>> Stashed changes
